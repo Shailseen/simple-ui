e5a3f94d9f9541e9feee0eae7cac276d37bcd052
Shailseen <shailesh13246@gmail.com>
2022-02-21 17:07:39 +0530

    feat: make responsive

--- doc-css/app.js
+++ doc-css/app.js
@@ -1,9 +1,21 @@
 const inputNumber = document.querySelector("#input-number");
-
+const floatingBtn = document.querySelector("#floating-button-main");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const sideBar = document.querySelector(".side-bar-main");
+const closeSideBar = document.querySelector("#close-button-side-bar");
 
 inputNumber.addEventListener("input",isInputValid);
-   
 
+floatingBtn.addEventListener("click",goToTop);
+
+hamburgerMenu.addEventListener("click",openDrawer);
+
+closeSideBar.addEventListener("click",handlerForCloseSideBar);
+
+
+function handlerForCloseSideBar(){
+    document.getElementById("side-bar-mobile").style.display = "none";
+}
 
 function isInputValid(){
     var inputNumberLength = inputNumber.value.length;
@@ -13,3 +25,14 @@ function isInputValid(){
     else
     inputNumber.style.color = "red";
 }
+
+function goToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var displayDrawer = false;
+function openDrawer(){
+    document.getElementById("side-bar-mobile").style.display = "block";
+}
+
